e35784984f7efd6c70453f7507380068a492702c
gopinath <gopinaths2301@gmail.com>
2023-07-07 03:40:21 -0700

    localstorage access

--- script/index.js
+++ script/index.js
@@ -1,13 +1,14 @@
 const textBox = document.getElementById('email');
-
+const emailInput = document.querySelector('.emailInput');
 
 function myFunction() {
     const emailInput = textBox.value;
+    window.localStorage.setItem('input', emailInput);
     let errorMsg = document.querySelector('.errorMsg');
     if (emailInput.includes("@") && emailInput.includes(".")) {
         textBox.classList.remove('emailError');
         errorMsg.style.display = "none";
-
+        window.location.href = "sucess.html";
         console.log('ok')
 
     }
@@ -18,4 +19,9 @@ function myFunction() {
         console.log('invalid')
 
     }
+    console.log(emailInput)
 }
+function dismiss() {
+    window.location.href = "index.html";
+}
+emailInput.innerHTML = localStorage.getItem("input")
